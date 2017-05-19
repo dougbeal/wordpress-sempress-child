5c60d0015aa93d2557a7ab619bed58c09ac9dfb0
Douglas Beal <git@dougbeal.com>
2017-05-18 22:35:21 -0700

    automatically approve bridgy comments

--- functions.php
+++ functions.php
@@ -12,4 +12,11 @@ add_action( 'init', 'dougbeal_unregister_categories' );
 unregister_widget( 'WP_Widget_Categories' );
 
 add_filter('amazon_affiliate_id', create_function('', 'return "dpb0e-20";'));
+
+
+
+function auto_approve_bridgy_comments($approved, $commentdata) {
+    return strpos($commentdata['comment_agent'], 'Bridgy (https://brid.gy/about) AppEngine-Google') !== false ? 1 : $approved;
+}
+add_filter('pre_comment_approved', 'auto_approve_bridgy_comments', '99', 2);
 ?>
