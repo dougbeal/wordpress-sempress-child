95f0087779af0b28461f5806af0d085d8acb2c5c
Douglas Beal <git@dougbeal.com>
2017-06-26 15:10:00 -0700

    add OwnYourSwarm auto-approve

--- functions.php
+++ functions.php
@@ -15,8 +15,31 @@ add_filter('amazon_affiliate_id', create_function('', 'return "dpb0e-20";'));
 
 
 
-function auto_approve_bridgy_comments($approved, $commentdata) {
-    return strpos($commentdata['comment_agent'], 'Bridgy (https://brid.gy/about) AppEngine-Google') !== false ? 1 : $approved;
+function auto_approve_some_comments($approved, $commentdata) {
+    if (strpos($commentdata['comment_agent'], 'Bridgy (https://brid.gy/about) AppEngine-Google') !== false ) {
+    # facebook doesn't get auto-approved due to privacy concerns
+        if (strpos($commentdata['comment_author_url'], 'facebook') !== false) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } elseif (strpos($commentdata['comment_author'], 'Swarm') !== false ) {
+        return 1;
+    }
 }
-add_filter('pre_comment_approved', 'auto_approve_bridgy_comments', '99', 2);
+    
+
+add_filter('pre_comment_approved', 'auto_approve_some_comments', '99', 2);
 ?>
+/*
+comment_author Swarm
+
+comment_author_url https://ownyourswarm.p3k.io
+https://ownyourswarm.p3k.io/checkin/595066358c35dc3d4717e29e/bf1b1322757d5644157b485c4fa5886a
+
+
+comment_author_url
+https://brid-gy.appspot.com/like/facebook
+
+
+*/
